09896c64ee2f58ec5c42733759d9d4b5111e6857
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-12-05 20:14:17 +0330

    Add Empty Dashboard

---
+++ app/(routes)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata:Metadata ={
+    title:'Dashboard'
+}
+
+export default function page() {
+  return (
+    <div className='h-full w-full text-center p-8 font-bold text-lg'>Dashboard Coming Soon!</div>
+  )
+}
