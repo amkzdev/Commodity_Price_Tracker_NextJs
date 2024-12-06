321a050e4a95218ecd51e218b591d6e5f9e1290b
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-12-06 18:57:04 +0330

    Disable legend

--- app/views/Chart/components/DataChart/DataChart.tsx
+++ app/views/Chart/components/DataChart/DataChart.tsx
@@ -44,7 +44,7 @@ export const DataChart = () => {
         },
         title: undefined,
         legend: {
-            enabled: true
+            enabled: false
         },
         // plotOptions: {
         //     area: {
