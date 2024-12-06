0fa59396d512e54341395d57a79e560806019e60
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-12-06 14:51:06 +0330

    Implement Static Data

--- app/staticData/index.ts
+++ app/staticData/index.ts
@@ -1,7 +1,19 @@
-export const commodities = ['gold' ,'silver' , 'platinum' , 'palladium']
+//Commodity
 
+export type CommodityType = 'gold' |'silver' | 'platinum' | 'palladium'
+export const commodities : Array<CommodityType> = ['gold' ,'silver' , 'platinum' , 'palladium']
+
+//Periods 
+export type PeriodType = 'live' | 'today' | 'week' | 'month' | '3 Month' | '6 Month' | 'Year' 
 export const periods =['live' , 'today' , 'week' , 'month' , '3 Month' , '6 Month' , 'Year' ]
 
+
+///Currency Type
+export type CurrencyType = 'British Pounds' | 'US Dollar' | 'Euro'
 export const currencies  = ['British Pounds' , 'US Dollar' , 'Euro']
 
+
+///Weight Type
+export type WeightType = 'grams' | 'ounces' | 'kilograms'
 export const weights = ['grams' , 'ounces' , 'kilograms']
+
