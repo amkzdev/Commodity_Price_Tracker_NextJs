b8260aeae4769576784a213c335301da684011df
Amirmahdi <amirmahdikeshanzare@gmail.com>
2024-12-11 16:10:20 +0330

    Implement Dynamic Title

--- app/(routes)/price/[...slug]/page.tsx
+++ app/(routes)/price/[...slug]/page.tsx
@@ -1,13 +1,27 @@
-import { commodities, commoditySymbols, CommodityType, periods, PeriodType } from '@staticData'
-import { pageProps } from '@types'
-import { Metadata } from 'next'
 import React from 'react'
+import { commodities, CommodityType, periods, PeriodType } from '@staticData'
+import { pageProps } from '@types'
+import { uppercaseFirstLetter } from '@utils'
+import clsx from 'clsx'
+import { Metadata } from 'next'
 import { Chart } from 'views/Chart/Chart'
 
+export async function generateMetadata ({params:{slug}} :pageProps<{ slug: [ CommodityType , PeriodType]}>): Promise<Metadata> {
 
-export const metadata: Metadata = {
-  title: 'Price Chart | Gold App'
+  const commodity:CommodityType = commodities.indexOf(slug[0])!=-1 ? slug[0] : 'gold'
+
+  const period:PeriodType=periods.indexOf(slug[1])!=-1 ?  slug[1] : 'today'
+
+  return ({ title:clsx(
+    (period != 'live' && period != 'today') && 'Last',
+    uppercaseFirstLetter(period ?? ''),
+    uppercaseFirstLetter(commodity ?? ''),
+    'Price Chart | Gold APP' 
+  )})
+
 }
+
+
 export default function page({ params :{slug} }: pageProps<{ slug: [ CommodityType , PeriodType]}>) {
 
   const commodity:CommodityType = commodities.indexOf(slug[0])!=-1 ? slug[0] : 'gold'
